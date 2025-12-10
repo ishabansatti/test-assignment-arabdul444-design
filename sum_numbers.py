158f6277c5d26b288e662a934b4a91cf59117d00
arabdul444-design <arabdul444@gmail.com>
2025-12-10 02:54:08 -0800

    Update sum_numbers.py

--- sum_numbers.py
+++ sum_numbers.py
@@ -1,8 +1,8 @@
-def sum_numbers(ags):
+def sum_numbers(a=2,b=3):
     """
     Returns the sum of all input numbers.
     If no arguments are passed, returns 0.
     """
     # TODO: implement the logic
-    # total =sum(2,3)
+    # total =sum(a+b)
     return total
